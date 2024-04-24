5f5d100d254272a6508a2bec920c2f5894194b2c
dydado13 <dydado13@gmail.com>
2024-04-24 16:28:51 +0200

    dsa

--- src/map_dup.c
+++ src/map_dup.c
@@ -6,11 +6,31 @@
 /*   By: dydado13 <dydado13@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/08 19:19:20 by lle-saul          #+#    #+#             */
-/*   Updated: 2024/04/04 15:31:45 by dydado13         ###   ########.fr       */
+/*   Updated: 2024/04/24 16:28:04 by dydado13         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/cube3d.h"
+
+char	**extra(char **cpy, char **tab)
+{
+	int	y;
+	int	x;
+
+	y = -1;
+	while (tab[++y])
+	{
+		x = -1;
+		while (tab[y][++x])
+		{
+			if (tab[y][x] == 'D')
+				cpy[y][x] = '0';
+			else
+				cpy[y][x] = tab[y][x];
+		}
+	}
+	return (cpy);
+}
 
 char	**map_dup(char **tab)
 {
@@ -30,12 +50,6 @@ char	**map_dup(char **tab)
 		cpy[y] = malloc(sizeof(char) * (x + 1));
 		cpy[y][x] = '\0';
 	}
-	y = -1;
-	while (tab[++y])
-	{
-		x = -1;
-		while (tab[y][++x])
-			cpy[y][x] = tab[y][x];
-	}
+	cpy = extra(cpy, tab);
 	return (cpy);
 }
